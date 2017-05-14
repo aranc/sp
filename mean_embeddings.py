3be75b1a037ff43297dcc7d6f445c3a6fa06f5ef
Aran Carmon <aran.carmon@gmail.com>
2017-05-15 01:27:48 +0300

    .

--- mean_embeddings.py
+++ mean_embeddings.py
@@ -5,6 +5,20 @@ try:
 except:
     print "Loading data."
     data = np.loadtxt("data/paragram-phrase-XXL.txt", dtype="object")
-    words = data[:,0].copy()
+    words = {_[1]:_[0] for _ in enumerate(data[:,0])}
     embeddings = data[:,1:].copy().astype("float64")
     guard = None
+
+def compute_mean_embeddings(sent):
+    sum_embeddings = 0
+    num_embeddings = 0
+    for word in sent.split():
+        if not word in words:
+            print "Ignoring unknown word: " +  word
+        else:
+            num_embeddings += 1
+            sum_embeddings = sum_embeddings + embeddings[words[word]]
+    if num_embeddings == 0:
+        return 0
+    return sum_embeddings / num_embeddings
+
